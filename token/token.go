5d2c52717f3a27e3e0bfb5bb77d2878562423fb0
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-01-31 22:29:18 +0900

    トークンを定義

---
+++ token/token.go
@@ -0,0 +1,48 @@
+// token/token.go
+
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	//トークンや文字が未知であるとき使う
+	ILLEGAL = "ILLEGAL"
+	//ファイル終端
+	EOF = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
