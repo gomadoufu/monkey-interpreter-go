7e801f8c234e83394d37298029cbf16c452ee696
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-02-23 10:54:13 +0900

    2.6.8 前置演算子

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"gomadoufu/monkey-interpreter-go/ast"
 	"gomadoufu/monkey-interpreter-go/lexer"
 	"gomadoufu/monkey-interpreter-go/token"
+	"strconv"
 )
 
 const (
@@ -34,6 +35,9 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 
 	//２つトークンを読み込む。curtokenとpeekTokenの両方がセットされる
 	p.nextToken()
@@ -44,6 +48,33 @@ func New(l *lexer.Lexer) *Parser {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+	return lit
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+
+	p.nextToken()
+
+	expression.Right = p.parseExpression(PREFIX)
+
+	return expression
 }
 
 func (p *Parser) nextToken() {
@@ -137,7 +168,6 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	return stmt
 }
 
-// Pratt構文解析器のための実装
 type (
 	prefixParseFn func() ast.Expression               // 前置構文解析関数
 	infixParseFn  func(ast.Expression) ast.Expression // 中置構文解析関数
@@ -163,9 +193,15 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	return stmt
 }
 
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
