fd0bf4747406b3d105e9f6ecddef0b03946a820a
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-04-08 11:13:09 +0900

    3.6 Ifの評価まで

---
+++ object/object.go
@@ -0,0 +1,40 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+// この言語に出現するすべての値の表現
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+// 整数型
+// この構造体の中に値を保持しておいて、この構造体への参照を引き回す
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+// 真偽値型
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+// NULL型
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
