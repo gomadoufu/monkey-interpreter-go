f542181ea190a26392b4428c0b5c601c9ed24ea1
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-05-23 23:34:07 +0900

    第3章終わり！！ 重かった〜〜〜〜

--- object/environment.go
+++ object/environment.go
@@ -1,5 +1,12 @@
 package object
 
+// 環境の拡張 環境を入れ子にする
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
 	return &Environment{store: s}
@@ -7,10 +14,16 @@ func NewEnvironment() *Environment {
 
 type Environment struct {
 	store map[string]Object
+	// 外側の環境への参照
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	// 環境が見つからない場合は外側の環境を探す
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
